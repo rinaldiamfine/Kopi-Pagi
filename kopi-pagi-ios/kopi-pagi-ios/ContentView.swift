989cd44caa8f5645417668879370a67b87cb20a3
Rinaldi <rinaldiamfine@gmail.com>
2021-09-24 15:24:45 +0700

    First Update

---
+++ kopi-pagi-ios/kopi-pagi-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kopi-pagi-ios
+//
+//  Created by Rinaldi LNU on 24/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kopi-pagi-ios/kopi-pagi-ios/kopi_pagi_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  kopi_pagi_iosApp.swift
+//  kopi-pagi-ios
+//
+//  Created by Rinaldi LNU on 24/09/21.
+//
+
+import SwiftUI
+
+@main
+struct kopi_pagi_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
